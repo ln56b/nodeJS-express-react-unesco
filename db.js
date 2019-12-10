d39d4f6af8db53e19ec950846ea789e9a3263e18
Hélène Baudrand <baudrand.helene@gmail.com>
2019-12-10 14:26:33 +0100

    all final routes

---
+++ db.js
@@ -0,0 +1,14 @@
+insert into site values 
+(null, 'Minaret and Archaeological Remains of Jam', 600, 'Afghanistan', 2002, 1, 'Earthern Architecture', '2019-12-09'),
+(null, 'Cultural Landscape and Archaeological Remains of the Bamiyan Valley', 1589265 , 'Afghanistan', 2003, 1, 'Cultural Landscape', '2019-12-09'),
+(null, 'Natural and Cultural Heritage of the Ohrid region', 15944 , 'Albania', 1979, 0, 'Cultural Landscape', '2019-12-09'),
+(null, 'Butrint', 8 , 'Albania', 1992, 0, 'Earthern Architecture', '2019-12-09'),
+(null, 'Al Qal a of Beni Hammad', 105 , 'Algeria', 1980, 0, 'Cultural Landscape', '2019-12-09'),
+(null, 'Djémila', 30 , 'Albania', 1982, 0, 'Earthern Architecture', '2019-12-09'),
+(null, 'Madriu-Perafita-Claror Valley', 4 , 'Andorra', 2004, 0, 'Cultural Landscape', '2019-12-09'),
+(null, 'Mbanza Kongo, Vestiges of the Capital of the former Kingdom of Kongo', 622 , 'Angola', 2017, 0, 'Cities', '2019-12-09'),
+(null, 'Antigua Naval Dockyard and Related Archaeological Sites', 3 , 'Antigua and Barbuda ', 2016, 0, 'Earthern Architecture ', '2019-12-09'),
+(null, 'Los Glaciares National Park', 726927 , 'Argentina', 1981, 0, 'Cultural Landscape', '2019-12-09'),
+(null, 'Iguazu National Park ', 55000 , 'Argentina', 1984, 0, 'Cultural Landscape', '2019-12-09'),
+(null, 'Monasteries of Haghpat and Sanahin', 2 , 'Armenia', 1996, 0, 'Earthern Architecture', '2019-12-09'),
+(null, 'Great Barrier Reef  ', 34870000, 'Australia', 1981, 0, 'Marine & coastal ', '2019-12-09')
---
+++ routes/index.js
@@ -0,0 +1,137 @@
+const { Router } = require('express');
+
+const router = Router();
+
+const connection = require('./conf');
+
+/* GET index page. */
+router.get('/', (req, res) => {
+  res.json({
+    title: 'UNESCO WORLD HERITAGE'
+  });
+});
+
+// 1. Get all sites
+router.get('/api/sites', (req, res) => {
+  let sql = 'SELECT * FROM site';
+  const sqlValues = [];
+  // 2. Filter country starts with
+  if (req.query.country) {
+    sql += ` WHERE country LIKE '${req.query.country}%'`;
+    sqlValues.push(req.query.country);
+  }
+  // 3. Filter site_name countains
+  if (req.query.site_name) {
+    sql += ` WHERE site_name LIKE '%${req.query.site_name}%' `;
+    sqlValues.push(req.query.site_name);
+  }
+  // 4. Filter inscription year > than
+  if (req.query.inscription_year) {
+    sql += ' WHERE inscription_year > ?';
+    sqlValues.push(req.query.inscription_year);
+  }
+
+  connection.query(sql, sqlValues, (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error getting this site');
+    } else if (results.length === 0) {
+      res.status(404).send('Site list not found');
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+// 5. Get all sites
+router.get('/api/sites/ordered', (req, res) => {
+  const sorter = 'site_name';
+  const getAllOrdered = `SELECT * FROM site ORDER BY ${sorter} ASC`;
+  connection.query(getAllOrdered, (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error getting the ordered sites');
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+// 6. Get one site from ID
+router.get('/api/sites/:id', (req, res) => {
+  const getOneSite = req.params.id;
+  connection.query('SELECT * FROM site WHERE id = ?;', [getOneSite], (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error getting this site ID');
+    } else {
+      res.json(results[0]);
+    }
+  });
+});
+
+// 7. Post a new site
+router.post('/api/sites', (req, res) => {
+  const siteData = req.body;
+  connection.query('INSERT INTO site SET ?', siteData, (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error saving a new site');
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
+
+// 8. Put a site from id
+router.put('/api/sites/:id', (req, res) => {
+  const siteData = req.body;
+  const idSite = req.params.id;
+  connection.query('UPDATE site SET ? WHERE id = ?', [siteData, idSite], (err) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error updating a new site');
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
+
+// 9. Put a site boolean is_in_danger 
+router.put('/api/sites/is-in-danger', (req, res) => {
+  connection.query(' UPDATE site SET is_in_danger = NOT is_in_danger', (err) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error updating a site boolean');
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
+
+// 10. Delete site from id
+router.delete('/api/sites/:id', (req, res) => {
+  const idSite = req.params.id;
+  connection.query('DELETE FROM site WHERE id = ?', [idSite], (err) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error deleting a site');
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
+
+// 11. Delete site if site_in_danger is false
+router.delete('/api/sites/indangered', (req, res) => {
+  connection.query('DELETE FROM site WHERE is_in_danger = false', (err) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error deleting a site');
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
+
+module.exports = router;
